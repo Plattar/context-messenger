4ae6498b253a4f90b0bc5c1720363082a254b7d8
David Arayan <david.arayan12@gmail.com>
2021-08-13 14:52:00 +1000

    - Added additional protection against multiple instances of the Messenger framework which seems to be causing erratic issues

--- context-messenger/index.js
+++ context-messenger/index.js
@@ -4,19 +4,30 @@ const Memory = require("./memory/memory.js");
 const GlobalEventHandler = require("./messenger/global-event-handler.js");
 const Version = require("./version");
 
-// create our instances which we only need one each
-const messengerInstance = new Messenger();
+// re-create a new messenger instance if it does not exist
+if (!GlobalEventHandler.instance().messengerInstance) {
+    // create our instances which we only need one each
+    const messengerInstance = new Messenger();
 
-// memory requires the messenger interface to function correctly
-const memoryInstance = new Memory(messengerInstance);
+    // memory requires the messenger interface to function correctly
+    const memoryInstance = new Memory(messengerInstance);
 
-GlobalEventHandler.instance().messengerInstance = messengerInstance;
-GlobalEventHandler.instance().memoryInstance = memoryInstance;
+    GlobalEventHandler.instance().messengerInstance = messengerInstance;
+    GlobalEventHandler.instance().memoryInstance = memoryInstance;
+}
+
+// re-create a new memory instance if it does not exist
+if (!GlobalEventHandler.instance().memoryInstance) {
+    // memory requires the messenger interface to function correctly
+    const memoryInstance = new Memory(GlobalEventHandler.instance().messengerInstance);
+
+    GlobalEventHandler.instance().memoryInstance = memoryInstance;
+}
 
 console.log("using @plattar/context-messenger v" + Version);
 
 module.exports = {
-    messenger: messengerInstance,
-    memory: memoryInstance,
+    messenger: GlobalEventHandler.instance().messengerInstance,
+    memory: GlobalEventHandler.instance().memoryInstance,
     version: Version
 }
